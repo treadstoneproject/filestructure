203094945f013f7fdfd83257183aa686dbc78c2a
Chatsiri <chatsiri@chatsiri.com>
2014-05-14 17:19:59 +0700

    Add first steps for view hex on filestructure. Welcome to The Matrix.

---
+++ format/hexdump.h
@@ -0,0 +1,20 @@
+#ifndef FILESTRUCTURE_HEXDUMP_H
+#define FILESTRUCTURE_HEXDUMP_H
+
+
+//Select type of files such PE, EXE, ELF and Processes in Linux & Win32 to hex dump format.
+class hexdump_processes{
+
+	
+
+};
+
+// Print hex value on file type.
+class hexdump_viewer{
+
+
+
+};
+
+
+#endif // !FILESTRUCTURE_HEXDUMP_H
